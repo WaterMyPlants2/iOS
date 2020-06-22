b700a3f660c27ed08e4d33f7f0713ea22d53fecc
Ezra Black <dev.ezrablack@gmail.com>
2020-06-22 11:31:00 -0500

    Added Base Project

---
+++ WMP34/WMP34/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WMP34
+//
+//  Created by Ezra Black on 6/22/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
