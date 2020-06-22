ba7c03a01c939cbd3e89ee17767b20ee557352aa
Ezra Black <dev.ezrablack@gmail.com>
2020-06-22 18:48:11 -0500

    Added Mock Data Complications

---
+++ WMP34/WMP34/URLSession.swift
@@ -0,0 +1,16 @@
+//
+//  URLSession.swift
+//  WMP34
+//
+//  Created by Ezra Black on 6/22/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+extension URLSession: NetworkDataLoader {
+    func loadData(using request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataTask(with: request) { (data, response, error) in
+            completion(data, response, error)
+        }.resume()
+    }
+}
