2ca6849c77204f951f6b55863a80fc13dd88aa4b
Brad <braddiroff@icloud.com>
2020-06-22 16:07:12 -0400

    Created 2 Groups

---
+++ WMP34/WMP34/Cells/MyPlantCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyPlantCollectionViewCell.swift
+//  WMP34
+//
+//  Created by Bradley Diroff on 6/22/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class MyPlantCollectionViewCell: UICollectionViewCell {
+    
+}
