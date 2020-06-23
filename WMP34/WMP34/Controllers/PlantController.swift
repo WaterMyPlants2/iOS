a9d3461683ef62b3692151cf6aae482b4f8adf17
Brad <braddiroff@icloud.com>
2020-06-23 13:49:20 -0400

    Added controllers

---
+++ WMP34/WMP34/Controllers/PlantController.swift
@@ -0,0 +1,50 @@
+//
+//  PlantController.swift
+//  WMP34
+//
+//  Created by Bradley Diroff on 6/23/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PlantController {
+    
+    let baseURL = URL(string: "https://jren-watermyplants.herokuapp.com/")!
+    
+    typealias CompletionHandler = (Result<Bool, NetworkError>) -> Void
+    
+    init() {
+        
+    }
+    
+    // MARK: - API Functions
+    
+    func fetchPlantsFromServer(completion: @escaping CompletionHandler = { _ in }) {
+        /*
+         GET - /api/plants - return a list of plants of current user
+         */
+    }
+    
+    func sendPlantToServer(plant: Plant, completion: @escaping CompletionHandler = { _ in }) {
+        /*
+         POST - /api/plants - add a plant given in the request body to current user
+         */
+    }
+    
+    func deletePlantFromServer(plant: Plant, completion: @escaping CompletionHandler = { _ in }) {
+        /*
+         DELETE - /api/plants/{plantid} - Delete the plant by plant id
+        */
+    }
+    
+    func updatePlantOnServer(plant: Plant, completion: @escaping CompletionHandler = { _ in }) {
+        /*
+         PUT - /api/plants/{plantid} - update a plant given in the request body
+         */
+    }
+    
+    // MARK: - Utility Functions
+}
+
