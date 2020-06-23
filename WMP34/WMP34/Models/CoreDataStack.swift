1bf616e6bf4677f7f573c8584ce897dd890859cd
Brad <braddiroff@icloud.com>
2020-06-23 13:38:50 -0400

    CoreData added

---
+++ WMP34/WMP34/Models/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  WMP34
+//
+//  Created by Bradley Diroff on 6/23/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tasks")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+}
+
---
+++ WMP34/WMP34/Models/User+Convenience.swift
@@ -0,0 +1,14 @@
+//
+//  User+Convenience.swift
+//  WMP34
+//
+//  Created by Bradley Diroff on 6/23/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension User {
+    
+}
