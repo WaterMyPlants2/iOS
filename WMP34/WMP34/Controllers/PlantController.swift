546c4ebe3b04195c06c5f3029dce36ea9272e0df
Vincent Hoang <vietnhoang7@gmail.com>
2020-06-24 15:40:01 -0700

    begin implementation of fetch plants task

--- WMP34/WMP34/Controllers/PlantController.swift
+++ WMP34/WMP34/Controllers/PlantController.swift
@@ -12,21 +12,56 @@ import CoreData
 class PlantController {
     
     let userController = UserController.shared
+    var plants: [PlantRepresentation] = []
     
     let baseURL = URL(string: "https://jren-watermyplants.herokuapp.com")!
     
     typealias CompletionHandler = (Result<Bool, NetworkError>) -> Void
     
     init() {
-        
+        fetchPlantsFromServer()
     }
     
     // MARK: - API Functions
     
     func fetchPlantsFromServer(completion: @escaping CompletionHandler = { _ in }) {
-        /*
-         GET - /api/plants - return a list of plants of current user
-         */
+
+        guard let token = userController.token?.access_token else {
+            NSLog("Token object was found to be nil, aborting")
+            return
+        }
+        
+        let requestUrl = URL(string: "/api/plants", relativeTo: baseURL)!
+        
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                NSLog("Error fetching plants: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from API")
+                completion(.failure(.noData))
+                return
+            }
+            
+            do  {
+                let plantRepresentations = Array(
+                    try JSONDecoder().decode([PlantRepresentation].self, from: data))
+                
+                try self.updatePlants(with: plantRepresentations)
+                self.plants = plantRepresentations
+            } catch {
+                NSLog("Error decoding plants from API: \(error)")
+                completion(.failure(.failedDecode))
+            }
+        }.resume()
     }
     
     func sendPlantToServer(plant: PlantRepresentation, completion: @escaping CompletionHandler = { _ in }) {
@@ -75,5 +110,15 @@ class PlantController {
     }
     
     // MARK: - Utility Functions
+    private func updatePlants(with representations: [PlantRepresentation]) throws {
+        /*
+         private func updatePlants()
+         
+         implement logic to update locally stored plants with data retrieved from api
+         */
+        
+        //let plantsToFetch = representations.compactMap { $0.nickname }
+        
+    }
 }
 
